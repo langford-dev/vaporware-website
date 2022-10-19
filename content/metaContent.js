9a4b898dae78c2e4da2c4844622831ffb566443c
langford <langfordquarshie21@gmail.com>
2022-10-19 08:30:09 +0000

    update: meta banner

--- content/metaContent.js
+++ content/metaContent.js
@@ -2,11 +2,11 @@ const metaContent = {
   title: "Vapourware Studios —— Home",
   description:
     "We are a talented team of developers and designers specialized in creating elegant websites, mobile apps, and designs that promote your business, increase conversions and drive awareness.",
-  image: "/assets/banner.png",
+  image: "/assets/meta/meta-banner.png",
   imageAlt: "Vapourware studios banner image",
   siteName: "vapourwarestudios.co",
   url: "https://www.vapourwarestudios.co",
-  twitterCreator: "@VapourwareHQ",
+  twitterCreator: "@VapourwareCo",
 };
 
 export default metaContent;
